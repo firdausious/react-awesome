41d81d83e9c69ddf4e8b917b96cb57552e1d5d8a
Firdaus <firdausious@gmail.com>
2020-10-06 20:44:15 +0700

    init component for S4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types'
+
 import logo from './logo.svg';
 import './App.css';
 
+const ReactDemo = () => {
+  const [username, setUsername] = useState('')
+
+  const loggingIn = () => setUsername('Riza')
+
+  const loggingOut = () => {
+    setUsername('')
+  }
+
+  return (
+    <div>
+      <span>{username}</span>
+
+      <br />
+      <button onClick={loggingIn}>Login</button>
+      <button onClick={loggingOut}>Logout</button>
+    </div>
+  )
+}
+
+const ListPeople = ({ peoples, a, b }) => (
+  <div>
+    <span>{a}</span>
+    <span>{b}</span>
+    <ul>
+      {peoples.map((p, index) => <li key={index}>{p.name}</li>)}
+    </ul>
+  </div>
+)
+
+ListPeople.propTypes = {
+  peoples: PropTypes.array.isRequired,
+  a: PropTypes.number,
+  b: PropTypes.number
+}
+
+const listPeople = [
+    {
+      name: 'Furqon',
+      status: 'Single'
+    },
+    {
+      name: 'Tya',
+      status: 'Single'
+    }
+]
+
 function App() {
+  const [peoples] = useState(listPeople)
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +69,12 @@ function App() {
         >
           Learn React
         </a>
+        <br/ >
+
+        <ReactDemo />
+        <br />
+
+        <ListPeople peoples={peoples} a={1} b={2} />
       </header>
     </div>
   );
